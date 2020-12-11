19ef6e44412d503cec56be15b732a08dcbd99c23
Mike McBride <mike.mcbride@hey.com>
2020-12-10 22:49:44 -0600

    cleanup day 10 part 1

--- 2020/day-10/solution-a.js
+++ 2020/day-10/solution-a.js
@@ -1,13 +1,9 @@
-const adapters = require('../../readInput')(__dirname).split('\n').map(x => parseInt(x, 10)).sort((a, b) => a - b)
-// we need to include the outlet and device in our chain
-const outlet = 0
-const device = Math.max(...adapters) + 3
-const chain = [outlet, ...adapters, device]
+const adapters = require('../../readInput')(__dirname).split('\n').concat(['0']).map(x => parseInt(x, 10)).sort((a, b) => a - b)
+adapters.push(Math.max(...adapters) + 3)
 const voltageDiffs = [null, 0, 0, 0]
-
-for (let i in chain) {
+for (let i in adapters) {
     if (i > 0) {
-        const diff = chain[i] - chain[i - 1]
+        const diff = adapters[i] - adapters[i - 1]
         voltageDiffs[diff] += 1
     }
 }
